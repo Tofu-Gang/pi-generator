93db227df6f167a7089571060c56d9d0eab2705d
Tofu Gang <tofugangsw@gmail.com>
2026-01-11 21:28:49 +0100

    figure out PI api and music theory library tonal

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,28 @@
+import { useEffect } from "react";
+import axios from "axios";
+import { Scale } from "tonal";
+
 function App() {
+    useEffect(() => {
+        async function fetchPi() {
+            await axios.get("https://api.pi.delivery/v1/pi?start=999990&numberOfDigits=10")
+                .then(function (response) {
+                    // handle success
+                    console.log("PRDEL!!!", response);
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                });
+        }
+
+        fetchPi();
+    }, []);
+    console.log("PRDEL!", Scale.names());
+    const key = "C";
+    const scale = "major";
+    console.log("PRDEL!!", Scale.scaleNotes(Scale.get(`${key} ${scale}`).notes));
+
     return (
         <>
             <h1>My App</h1>
