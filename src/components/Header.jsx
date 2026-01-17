047e24360ac77983223383fd3d7ce956ece75a96
Tofu Gang <tofugangsw@gmail.com>
2026-01-18 00:25:05 +0100

    nicer header breadcrumbs

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,47 @@
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
 import { useMusic } from "../context/music.jsx";
 
 function Header() {
-    const { key, filters, scale, notes, resultLength, resultNotes } = useMusic();
-    const breadcrumbs = [];
-
-    if(key) {
-        breadcrumbs.push(`Key: ${key}`);
-    }
-    if(filters.some((filter) => filter.checked)) {
-        breadcrumbs.push(`Filters: ${filters.filter((filter) => filter.checked).map((filter) => filter.name).join(", ")}`);
-    }
-    if(scale) {
-        breadcrumbs.push(`Scale: ${scale.name}`);
-    }
-    if(notes.length > 0) {
-        breadcrumbs.push(`Notes: ${notes.join(", ")}`);
-    }
-    if(resultLength) {
-        breadcrumbs.push(`${resultLength.name}: ${resultLength.length}`);
-    }
-    if(resultNotes.length > 0) {
-        breadcrumbs.push(`Result: ${resultNotes}`);
-    }
+    const { key, filters, scale, resultLength, resultNotes } = useMusic();
+    const breadcrumbs = [
+        {
+            name: "Key",
+            done: !!key
+        },
+        {
+            name: "Filters",
+            done: filters.some((filter) => filter.checked)
+        },
+        {
+            name: "Scale",
+            done: !!scale
+        },
+        {
+            name: "Result Length",
+            done: !!resultLength
+        },
+        {
+            name: "Result Notes",
+            done: resultNotes.length > 0
+        }];
+    const location = useLocation();
 
     return (
         <div style={{display: "flex", border: "4px solid red"}}>
-            <h1>HEADER</h1>
-            <h1><Link to="/">Home</Link></h1>
-            {breadcrumbs.length > 0 && <p>{breadcrumbs.join("| ")}</p>}
+            {location.pathname === "/" && <h3 style={{padding: "3px"}}>BREADCRUMBS:</h3>}
+            {location.pathname !== "/" && <><h3 style={{padding: "3px"}}><Link to="/">Home</Link></h3><h3 style={{padding: "3px"}}>BREADCRUMBS:</h3></>}
+            {breadcrumbs.length > 0 && breadcrumbs.map((breadcrumb) =>
+                <h3
+                    key={breadcrumb.name}
+                    style={{
+                        color: breadcrumb.done ? "green" : "gray",
+                        border: `1px solid ${breadcrumb.done ? "green" : "gray"}`,
+                        padding: "3px"
+                    }}
+                >
+                    {breadcrumb.name}
+                </h3>
+            )}
         </div>
     );
 }
