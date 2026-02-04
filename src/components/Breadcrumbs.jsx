79c5aa18b9a9b1f8e854933bb595f9688630a508
Tofu Gang <tofugangsw@gmail.com>
2026-02-04 21:17:40 +0100

    make breadcrumb button background yellow/green if the current page is the breadcrumb button

--- src/components/Breadcrumbs.jsx
+++ src/components/Breadcrumbs.jsx
@@ -1,20 +1,32 @@
 import { useMusic } from "../context/music.jsx";
-import { PiGeneratorRoutes } from "../App.jsx";
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
 
 function Breadcrumbs() {
     const {links} = useMusic();
+    const location = useLocation();
 
     return (
         <>
             <div className="flex flex-wrap">
                 {links.map((breadcrumb) =>
                     <Link
-                        className={`m-1 rounded-full
-                    ${breadcrumb.state.done() ? "text-green-600" : breadcrumb.state.available() ? "text-yellow-600 animate-pulse" : "text-gray-600"} 
-                    border
-                    ${breadcrumb.state.done() ? "border-green-600" : breadcrumb.state.available() ? "border-yellow-600" : "border-gray-600"}
-                    p-3 ${!breadcrumb.state.done() && !breadcrumb.state.available() && "pointer-events-none"}`}
+                        className={`m-1 p-3 rounded-full
+                        ${breadcrumb.state.done() ? 
+                            breadcrumb.path === location.pathname ? 
+                                "text-black bg-green-600" : 
+                                "text-green-600" : 
+                            breadcrumb.state.available() ? 
+                                breadcrumb.path === location.pathname ? 
+                                    "text-black bg-yellow-600" : 
+                                    "text-yellow-600 animate-pulse" : 
+                                "text-gray-600"}
+                        border
+                        ${breadcrumb.state.done() ? 
+                            "border-green-600" : 
+                            breadcrumb.state.available() ? 
+                                "border-yellow-600" : 
+                                "border-gray-600"}
+                        ${!breadcrumb.state.done() && !breadcrumb.state.available() && "pointer-events-none"}`}
                         key={breadcrumb.display}
                         to={breadcrumb.path}
                     >
