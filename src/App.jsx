0296f236695f38c91f9a2a292c9c5a9bb7e33a2b
Tofu Gang <tofugangsw@gmail.com>
2026-03-27 16:49:27 +0100

    Modify layout of the central page.

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,32 @@ import Filters from "./components/pages/Filters.jsx";
 import Scale from "./components/pages/Scale.jsx";
 import Length from "./components/pages/Length.jsx";
 import Result from "./components/pages/Result.jsx";
+import {useMusic} from "./context/music.jsx";
 
 function App() {
+    const { key, filters, scale, resultLength } = useMusic();
+
     return (
         <div>
             <Header />
             <div className="flex flex-col">
-                <div className="flex justify-between">
-                    <Key />
-                    <Filters />
-                    <Scale />
-                    <Length />
+                <div className={`${key.done() && filters.done() && scale.done() && resultLength.done() ? "visible" : "hidden"}`}>
+                    <Result />
                 </div>
-                <Result />
+                <div className="flex justify-between">
+                    <div>
+                        <Key />
+                    </div>
+                    <div className={`${key.done() ? "visible" : "invisible"}`}>
+                        <Filters />
+                    </div>
+                    <div className={`${key.done() && filters.done() ? "visible" : "invisible"}`}>
+                        <Scale />
+                    </div>
+                    <div className={`${key.done() && filters.done() && scale.done() ? "visible" : "invisible"}`}>
+                        <Length />
+                    </div>
+                </div>
             </div>
             <Footer />
         </div>
