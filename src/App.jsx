f49ea79d9d9aff40393401cc5b8471d5ba026f03
Tofu Gang <tofugangsw@gmail.com>
2026-04-07 14:19:37 +0200

    Make a TODO list of frontend changes.

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,17 @@ import Scale from "./components/pages/Scale.jsx";
 import Length from "./components/pages/Length.jsx";
 import Result from "./components/pages/Result.jsx";
 import { useMusic } from "./context/music.jsx";
-
+// TODO: send request automatically, don't wait for the user to click the generate (FETCH) button!
+// TODO: add random button
+// User does not immediately understand what the app does, what is the meaning of all settings, risk of immediate leave!
+// Research what does header and footer contain normally and use these patterns!
+// Cultivate the Matrix style, add some fancy animations for example parts of PI number, with neon effect and so on.
+// Keep the "old school" style, rectangles without rounded corners and so on
+// Title/Home page is missing, develop it and clearly explain what is the purpose of the app, what it does, how does it
+// work, what input is needed, maybe add a quick call to action button and let the user know that they do not need to
+// read all the text, the UX will guide them
+// Rethink the app layout: The four columns should be hidden and considered a "setting", have one button to display or
+// close it-on mobile layout. For large screens, implement left-side menu (example can be alza.cz).
 function App() {
     const { Status } = useMusic();
 
